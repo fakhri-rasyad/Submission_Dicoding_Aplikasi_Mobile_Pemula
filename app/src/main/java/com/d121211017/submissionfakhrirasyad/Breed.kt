8cd20098d919e6326794151588c992d0b5f62329
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-02-25 15:28:23 +0800

    feat: fungsi untuk mengambil data dari string.xml

---
+++ app/src/main/java/com/d121211017/submissionfakhrirasyad/Breed.kt
@@ -0,0 +1,16 @@
+package com.d121211017.submissionfakhrirasyad
+
+import android.os.Parcelable
+import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Breed(
+    val name: String,
+    val color: String,
+    val weight: String,
+    val size: String,
+    val coat: String,
+    @DrawableRes
+    val photo: Int,
+) : Parcelable
--- app/src/main/java/com/d121211017/submissionfakhrirasyad/MainActivity.kt
+++ app/src/main/java/com/d121211017/submissionfakhrirasyad/MainActivity.kt
@@ -2,10 +2,41 @@ package com.d121211017.submissionfakhrirasyad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.d121211017.submissionfakhrirasyad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private val catBreedList = ArrayList<Breed>()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recycleViewMain
+        recyclerView.setHasFixedSize(true)
+
+        catBreedList.addAll(getCatBreedData())
+    }
+
+    private fun getCatBreedData() : ArrayList<Breed>{
+        val breedName = resources.getStringArray(R.array.cat_breed_name)
+        val breedSize = resources.getStringArray(R.array.cat_breed_size)
+        val breedCoat = resources.getStringArray(R.array.cat_breed_coat)
+        val breedColor = resources.getStringArray(R.array.cat_breed_color)
+        val breedWeight = resources.getStringArray(R.array.cat_breed_weight)
+        val breedPhoto = resources.getIntArray(R.array.cat_breed_photo)
+
+        val catBreed = ArrayList<Breed>()
+
+        for(i in breedName.indices){
+            val breed = Breed(breedName[i], breedColor[i], breedWeight[i], breedSize[i], breedCoat[i], breedPhoto[i])
+            catBreed.add(breed)
+        }
+
+        return catBreed
     }
 }
